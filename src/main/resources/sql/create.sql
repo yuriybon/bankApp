b978fa057571701b91002a9f953518172eb26eff
bondary <DCTghjcnj_01>
2019-06-27 09:11:29 +0200

    Database initialisation scripts

---
+++ src/main/resources/sql/create.sql
@@ -0,0 +1,16 @@
+create table BANK(
+id number,
+name varchar(50)
+);
+
+CREATE TABLE ACCOUNT (
+  id number AUTO_INCREMENT,
+  balance number
+);
+
+CREATE TABLE PARTY (
+  id number,
+  party_name varchar2(255),
+  party_type varchar2(8)
+)
+;
